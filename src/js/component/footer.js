a06b019b4eed4155b7af57d9be3730fb911d74e4
Sheika1404 <rosheika1404@gmail.com>
2021-06-26 05:11:52 +0000

    initial commit

---
+++ src/js/component/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <nav class="navbar fixed-bottom navbar-dark bg-dark">
+            <div class="container-fluid">
+               <p>Copyright &copy; Your Website, 2018</p> 
+            </div>
+        </nav>
+    )
+
+}
+export default Footer;
