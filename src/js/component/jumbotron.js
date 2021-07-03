8376c064244843913432c60f1ba5433a9e1cbecf
Sheika1404 <rosheika1404@gmail.com>
2021-07-03 01:10:52 +0000

    finished

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = () => {
 	return (
-		<div className="jumbotron">
+		<div className="jumbotron text-center">
 			<h1 className="display-2">A Warm Welcome!</h1>
 			<p className="lead">
 				This is a simple hero unit, a simple jumbotron-style component
